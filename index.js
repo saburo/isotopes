3eab5f9eb692496fcce68f2d339727640afe1379
Kouki Kitajima <saburo@geology.wisc.edu>
2017-11-27 11:16:51 -0600

    Modify header from plain text to html

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World from Node.js\n');
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.end('<p>Hello World from <strong>Node.js</strong></p>');
 }).listen(3000);
 
 console.log('Server running at http://127.0.0.1:3000');
