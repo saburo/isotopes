c860154a2afa70738f172b96d2374e080d15207f
Glitch (isotopes) <none>
2017-11-27 17:23:57 +0000

    🙍🖐 Updated with Glitch

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 var http = require('http');
+var tmp = 'node.js';
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/html'});
-  response.end('<p>Hello World from <strong>Node.js</strong></p>');
-}).listen(3000);
+  response.write(`
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Document</title>
+    </head>
+    <body>
+      <p>Hello World from <strong>${tmp}</strong></p>
+    </body>
+  </html>
+  `);
+  response.end();
+}).listen(8888);
 
-console.log('Server running at http://127.0.0.1:3000');
+console.log('Server running at http://127.0.0.1:8888');
